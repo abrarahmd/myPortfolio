ab7dca5532a61050d98c24f5be13fb56c9140802
Abrar Ahmed <abrargroad2000@gmail.com>
2023-09-19 23:07:43 -0600

    MyPortfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+// Typing Effect
+let typed = new Typed(".auto-input", {
+    strings: ['Undergratuate', "FullStack Developer","AI Developer"],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay: 2000,
+    loop: true,
+})
