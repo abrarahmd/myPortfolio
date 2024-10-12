f94783f2b5346197292193aa935a90bb40035362
Abrar Ahmed <abrargroad2000@gmail.com>
2024-10-12 14:06:00 +0600

    Details Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Typing Effect
 let typed = new Typed(".auto-input", {
-    strings: ['on my 9th semester', 'Undergratuate', 'FullStack Developer', 'doing thesis on NLP'],
+    strings: ['on my 11th semester', 'Undergratuate', 'FullStack Developer', 'AI & ML Developer', 'doing thesis on NLP'],
     typeSpeed: 100,
     backSpeed:100,
     backDelay: 2000,
